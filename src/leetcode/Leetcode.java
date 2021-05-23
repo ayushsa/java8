33226f3deab8b1411bcb7d73b73caf200404ce2c
ayush099 <saxena.ayush09@gmail.com>
2021-05-23 20:16:10 +0530

    CtoJava Java To C style variable

--- src/leetcode/Leetcode.java
+++ src/leetcode/Leetcode.java
@@ -11,9 +11,81 @@ public class Leetcode
 
     public static void main(String[] args)
     {
+        // 1
         javaToCToJava("this_is_a_variable");
-
         javaToCToJava("thisIsAVariable");
+
+        //2
+        // https://www.geeksforgeeks.org/majority-element/
+        int arr[] = { 5, 1, 2, 5, 5, 5, 1 };
+        int n = arr.length;
+        // Function calling
+        findMajority(arr, n);
+
+        //3 Print the frequency of each character in Alphabetical order
+        String s = "geeksforgeeks";
+        compressString(s);
+    }
+
+    private static void compressString(String s)
+    {
+        // To store the frequency
+        // of the characters
+        int freq[] = new int[26] ;
+
+        // Update the frequency array
+        for (int i = 0; i < s.length(); i++)
+        {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        // Print the frequency in alphatecial order
+        for (int i = 0; i < 26; i++)
+        {
+            // If the current alphabet doesn't
+            // appear in the string
+            if (freq[i] == 0)
+                continue;
+
+            System.out.print((char)(i + 'a') +""+ freq[i]);
+        }
+    }
+
+    private static void findMajority(int[] arr, int n)
+    {
+        int maxCount = 0;
+        int index = -1;
+
+        for (int i = 0; i < n; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (arr[i] == arr[j])
+                {
+                    count++;
+                }
+            }
+
+            // update maxCount if count of
+            // current element is greater
+            if (count > maxCount)
+            {
+                maxCount = count;
+                index = i;
+            }
+        }
+
+        // if maxCount is greater than n/2
+        // return the corresponding element
+        if (maxCount > n / 2)
+        {
+            System.out.println(arr[index]);
+        }
+        else
+        {
+            System.out.println("No Majority Element");
+        }
     }
 
     // input : this_is_a_variable Output: thisIsAVariable using JAVA program
